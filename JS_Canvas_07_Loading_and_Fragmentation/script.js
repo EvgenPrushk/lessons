eef71e0d2296440108a1f636e0868041bed49b18
Evgen Prushak <ogmprushak@mail.ru>
2020-12-21 00:14:32 +0300

    feat: add task for lesson 7

--- JS_Canvas_07_Loading_and_Fragmentation/script.js
+++ JS_Canvas_07_Loading_and_Fragmentation/script.js
@@ -3,13 +3,47 @@ const context = canvas.getContext("2d");
 
 canvas.width = 500;
 canvas.height = 500;
+const CELL_SIZE = 1;
 
-context.beginPath();
-context.moveTo(300, 400);
-context.lineTo(250, 250);
-context.lineTo(320, 100);
-// возвращает в начальную точку (замыкает фигуру)
-context.closePath();
-// context.fillStyle = "green"
-// context.fill();
-context.stroke();
+main();
+
+async function main() {
+    const city = await loadImage("sets/city.jpg");
+    console.log(city.width, city.height);
+    canvas.width = city.width;
+    canvas.height = city.height;
+    
+    for (let y = 0; y < city.height / CELL_SIZE; y++) {
+        for (let x = 0; x < city.width/ CELL_SIZE; x++) {
+            if ((x + y) % 2 === 0) {
+                continue;
+            }
+
+            context.drawImage(
+                city,
+                x * CELL_SIZE,
+                y * CELL_SIZE,
+                CELL_SIZE,
+                CELL_SIZE,
+                x * CELL_SIZE,
+                y * CELL_SIZE,
+                CELL_SIZE,
+                CELL_SIZE,
+            )
+            
+        }
+        
+    }
+
+// 1 - само изображение, 2 и 3 -координаты x и у верхнего угла, 4 и 5 - ширина и высота из изображения
+//   6 и 7 координаты x и у верхнего угла, 8 и 9 - ширина и высота куда мы хотить вписать выбранное изображение
+//    context.drawImage(city, 141, 50, 55, 113, 5, 5, 100, 125);
+}
+
+function loadImage (src) {
+    return new Promise((resolve) =>{
+        const image = new Image();
+        image.src = src;
+        image.onload = () => resolve(image);
+    });
+}
