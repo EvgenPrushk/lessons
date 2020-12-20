b4a08bcad6c2479a3c95a532da22a96b16dccbce
Evgen Prushak <ogmprushak@mail.ru>
2020-12-21 01:23:07 +0300

    feat: add animation triangle

--- JS_Canvas_08_Animation_Basics/script.js
+++ JS_Canvas_08_Animation_Basics/script.js
@@ -4,12 +4,42 @@ const context = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 500;
 
-context.beginPath();
-context.moveTo(300, 400);
-context.lineTo(250, 250);
-context.lineTo(320, 100);
-// возвращает в начальную точку (замыкает фигуру)
-context.closePath();
-// context.fillStyle = "green"
-// context.fill();
-context.stroke();
+const triangle = {
+    centerX: canvas.width / 2,
+    centerY: canvas.height / 2,
+    radius: 150,
+    angle: 0,
+    angleSpped: Math.PI * 0.01,
+}
+
+animation({
+  clear() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  },
+
+  update() {
+    triangle.angle += triangle.angleSpped;
+  },
+
+  render() {
+      const dAngle = (Math.PI * 2) / 3;
+
+    context.beginPath();
+    context.moveTo(
+        triangle.centerX + triangle.radius * Math.cos(triangle.angle),
+        triangle.centerY + triangle.radius * Math.sin(triangle.angle),
+    );
+    context.lineTo(
+        triangle.centerX + triangle.radius * Math.cos(triangle.angle + dAngle),
+        triangle.centerY + triangle.radius * Math.sin(triangle.angle + dAngle),
+    );
+    context.lineTo(
+        triangle.centerX + triangle.radius * Math.cos(triangle.angle + 2 * dAngle),
+        triangle.centerY + triangle.radius * Math.sin(triangle.angle + 2 * dAngle),
+    );
+    context.closePath();
+   
+    context.fillStyle = "yellow";
+    context.fill();
+  },
+});
