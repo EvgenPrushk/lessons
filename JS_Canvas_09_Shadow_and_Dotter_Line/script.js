1c3f224a2a04231f9a0636a663c4256dd58d2bdf
Прушак Евгений Владимирович <D:\poshta>
2020-12-21 17:31:23 +0300

    feat: add function clear, render, update

--- JS_Canvas_09_Shadow_and_Dotter_Line/script.js
+++ JS_Canvas_09_Shadow_and_Dotter_Line/script.js
@@ -4,12 +4,103 @@ const context = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 500;
 
-context.beginPath();
-context.moveTo(300, 400);
-context.lineTo(250, 250);
-context.lineTo(320, 100);
-// возвращает в начальную точку (замыкает фигуру)
-context.closePath();
-// context.fillStyle = "green"
-// context.fill();
-context.stroke();
+// let shadowBlur = 0;
+// let shadowOffsetX = 0;
+// let shadowOffsetY = 0;
+// let lineDashOffset = 0;
+
+// const TIME_SCALE = 10;
+
+// animation({
+//     clear() {
+//         canvas.width |= 0;
+//     },
+
+//     update( {timestamp}) {
+//         shadowBlur = 10 + 10 * Math.cos(timestamp * TIME_SCALE);
+//         shadowOffsetX = 10 + 10 * Math.cos(timestamp * TIME_SCALE);
+//         shadowOffsetY = 10 + 10 * Math.sin(timestamp * TIME_SCALE);
+//     },
+
+//     render() {
+//         context.beginPath();
+//         context.rect(100, 180, 250, 250)
+//         context.shadowColor = "red";
+//         context.shadowBlur = shadowBlur;
+//         context.shadowOffsetX = shadowOffsetX;
+//         context.shadowOffsetY = shadowOffsetY;
+//         context.strokeStyle = "blue";
+//         context.stroke();
+//     },
+// });
+// const dash = Array(100).fill().map((_, i) => i * 2 + 1);
+
+// animation({
+//     clear() {
+//         canvas.width |= 0;
+//     },
+
+//     update({ secondPart }) {
+//         lineDashOffset += secondPart * TIME_SCALE;
+//     },
+
+//     render() {
+//         context.beginPath();
+//         context.setLineDash(dash);
+//         context.lineDashOffset = lineDashOffset;
+//         context.lineWidth = 5;
+//         context.rect( 100, 180, 300, 300);
+//         context.stroke();
+       
+//     },
+// });
+
+// function drawLine(y) {
+//     context.beginPath();
+//     context.moveTo(0, y);
+//     context.lineTo(500, y);
+//     context.stroke();
+// }
+
+let  miterLimit = 100;
+
+animation({
+    clear() {
+        canvas.width  |= 0;
+    },
+
+    update({timestamp}) {
+        miterLimit = 100 - 100 * Math.cos(timestamp);
+    },
+
+    render() {
+        context.beginPath();
+        context.moveTo(0, canvas.height / 2);
+        for (let i = 1; i <= 100; i++) {
+            const y = canvas.height / 2 + 100 * (- 1) ** i;
+            context.lineTo(i ** 2, y);            
+        }
+        context.lineWidth = 5;
+        context.miterLimit = miterLimit;
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(0, canvas.height / 2 - 75);
+        context.lineTo(canvas.width, canvas.height / 2 - 75);
+        context.lineWidth = 1;
+        context.strokeStyle = "blue";
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(0, canvas.height / 2 + 75);
+        context.lineTo(canvas.width, canvas.height / 2 + 75);
+        context.lineWidth = 1;
+        context.strokeStyle = "blue";
+        context.stroke();
+
+        context.fillText(miterLimit, canvas.width, 0);
+        context.font = "50px solid gray";
+        context.textBaseline = "top";
+        context.textAlign = "right";
+    },
+});
