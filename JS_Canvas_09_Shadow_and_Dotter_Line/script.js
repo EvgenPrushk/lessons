a03061a1ba2c81b0b016f2c8f2c6644cf413330f
Прушак Евгений Владимирович <D:\poshta>
2020-12-22 17:13:39 +0300

    feat: add task for lesson 9

--- JS_Canvas_09_Shadow_and_Dotter_Line/script.js
+++ JS_Canvas_09_Shadow_and_Dotter_Line/script.js
@@ -51,7 +51,7 @@ canvas.height = 500;
 //         context.lineWidth = 5;
 //         context.rect( 100, 180, 300, 300);
 //         context.stroke();
-       
+
 //     },
 // });
 
@@ -62,45 +62,102 @@ canvas.height = 500;
 //     context.stroke();
 // }
 
-let  miterLimit = 100;
+// let miterLimit = 100;
+// const TIME_SCALE = 0.0005;
+
+// animation({
+//     clear() {
+//         canvas.width |= 0;
+//     },
+
+//     update({ timestamp }) {
+//         miterLimit = 100 - 100 * Math.cos(timestamp * TIME_SCALE);
+//     },
+
+//     render() {
+//         context.beginPath();
+//         context.moveTo(0, canvas.height / 2);
+
+//         for (let i = 1; i <= 100; i++) {
+//             const y = canvas.height / 2 + 100 * (-1) ** i;
+//             context.lineTo( i ** 2, y);
+//         }
+
+//         context.lineWidth = 5;
+//         context.miterLimit = miterLimit;
+//         context.stroke();
+
+//         context.beginPath();
+//         context.moveTo(0, canvas.height / 2 - 75);
+//         context.lineTo(canvas.width, canvas.height / 2 - 75);
+//         context.strokeStyle = "blue";
+//         context.lineWidth = 1;
+//         context.stroke();
+
+//         context.beginPath();
+//         context.moveTo(0, canvas.height / 2 + 75);
+//         context.lineTo(canvas.width, canvas.height / 2 + 75);
+//         context.strokeStyle = "blue";
+//         context.lineWidth = 1;
+//         context.stroke();
+
+//         context.fillText(miterLimit, canvas.width, 0);
+//         context.font = "50px solid gray"
+//         context.textBaseline = "top";
+//         context.textAlign = "right";
+//     }
+// });
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const R = 200;
+const r = 100;
+
+let angle = 0;
+const ANGLE_SPEED = Math.PI * 0.1;
+
+let lineDashOffset = 0;
 
 animation({
     clear() {
-        canvas.width  |= 0;
+        canvas.width |= 0;
     },
 
-    update({timestamp}) {
-        miterLimit = 100 - 100 * Math.cos(timestamp);
+    update({ timestamp, secondPart }) {
+        angle = Math.PI * 0.25 * Math.cos(timestamp * 0.001);
+        lineDashOffset -= secondPart * 10;
     },
 
     render() {
+        const dAngle = Math.PI / 5;
+
+
         context.beginPath();
-        context.moveTo(0, canvas.height / 2);
-        for (let i = 1; i <= 100; i++) {
-            const y = canvas.height / 2 + 100 * (- 1) ** i;
-            context.lineTo(i ** 2, y);            
+        context.moveTo(x + R * Math.cos(angle), y + R * Math.sin(angle));
+
+        for (let i = 1; i < 10; i++) {
+            const radius = i % 2 ? r : R;
+            context.lineTo(
+                x + radius * Math.cos(angle + dAngle * i),
+                y + radius * Math.sin(angle + dAngle * i),
+            );
+
         }
-        context.lineWidth = 5;
-        context.miterLimit = miterLimit;
+
+        context.closePath();
+        context.lineWidth = 3;
+        context.lineDashOffset = lineDashOffset;
+        context.setLineDash([10, 10]);
         context.stroke();
 
         context.beginPath();
-        context.moveTo(0, canvas.height / 2 - 75);
-        context.lineTo(canvas.width, canvas.height / 2 - 75);
-        context.lineWidth = 1;
+        context.arc(x, y, R + 2, 0, Math.PI * 2);
+        context.lineWidth = 2;
         context.strokeStyle = "blue";
+        context.setLineDash([100, 100]);
         context.stroke();
-
-        context.beginPath();
-        context.moveTo(0, canvas.height / 2 + 75);
-        context.lineTo(canvas.width, canvas.height / 2 + 75);
-        context.lineWidth = 1;
-        context.strokeStyle = "blue";
-        context.stroke();
-
-        context.fillText(miterLimit, canvas.width, 0);
-        context.font = "50px solid gray";
-        context.textBaseline = "top";
-        context.textAlign = "right";
     },
-});
+
+
+})
