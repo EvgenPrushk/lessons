975f75844e6b375c91826977dfd25c7015e543eb
Прушак Евгений Владимирович <D:\poshta>
2020-12-29 17:00:38 +0300

    feat: add task 8

--- JS_Canvas_08_Animation_Basics/script.js
+++ JS_Canvas_08_Animation_Basics/script.js
@@ -4,42 +4,44 @@ const context = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 500;
 
-const triangle = {
-    centerX: canvas.width / 2,
-    centerY: canvas.height / 2,
-    radius: 150,
-    angle: 0,
-    angleSpped: Math.PI * 0.01,
-}
+// const triangle = {
+//     centerX: canvas.width / 2,
+//     centerY: canvas.height / 2,
+//     radius: 150,
+//     angle: 0,
+//     angleSpped: Math.PI * 0.01,
+// }
 
-animation({
-  clear() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-  },
+// animation({
+//   clear() {
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+//   },
 
-  update() {
-    triangle.angle += triangle.angleSpped;
-  },
+//   update() {
+//     triangle.angle += triangle.angleSpped;
+//   },
 
-  render() {
-      const dAngle = (Math.PI * 2) / 3;
+//   render() {
+//       const dAngle = (Math.PI * 2) / 3;
 
-    context.beginPath();
-    context.moveTo(
-        triangle.centerX + triangle.radius * Math.cos(triangle.angle),
-        triangle.centerY + triangle.radius * Math.sin(triangle.angle),
-    );
-    context.lineTo(
-        triangle.centerX + triangle.radius * Math.cos(triangle.angle + dAngle),
-        triangle.centerY + triangle.radius * Math.sin(triangle.angle + dAngle),
-    );
-    context.lineTo(
-        triangle.centerX + triangle.radius * Math.cos(triangle.angle + 2 * dAngle),
-        triangle.centerY + triangle.radius * Math.sin(triangle.angle + 2 * dAngle),
-    );
-    context.closePath();
+//     context.beginPath();
+//     context.moveTo(
+//         triangle.centerX + triangle.radius * Math.cos(triangle.angle),
+//         triangle.centerY + triangle.radius * Math.sin(triangle.angle),
+//     );
+//     context.lineTo(
+//         triangle.centerX + triangle.radius * Math.cos(triangle.angle + dAngle),
+//         triangle.centerY + triangle.radius * Math.sin(triangle.angle + dAngle),
+//     );
+//     context.lineTo(
+//         triangle.centerX + triangle.radius * Math.cos(triangle.angle + 2 * dAngle),
+//         triangle.centerY + triangle.radius * Math.sin(triangle.angle + 2 * dAngle),
+//     );
+//     context.closePath();
    
-    context.fillStyle = "yellow";
-    context.fill();
-  },
-});
+//     context.fillStyle = "yellow";
+//     context.fill();
+//   },
+// });
+
+const stars = [];
