6cfb29228c524e3d6d575e0aa34d68b2cebcaf27
Прушак Евгений Владимирович <D:\poshta>
2020-12-30 20:27:48 +0300

    feat: add function main

--- JS_Canvas_10_Fill_Pattern/script.js
+++ JS_Canvas_10_Fill_Pattern/script.js
@@ -4,12 +4,43 @@ const context = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 500;
 
-context.beginPath();
-context.moveTo(300, 400);
-context.lineTo(250, 250);
-context.lineTo(320, 100);
-// возвращает в начальную точку (замыкает фигуру)
-context.closePath();
-// context.fillStyle = "green"
-// context.fill();
-context.stroke();
+main();
+async function main() {
+    const image = await loadImage(
+        "https://mdn.mozillademos.org/files/222/Canvas_createpattern.png"
+    );
+    
+    const patter = context.createPattern(image, "repeat");
+    context.fillStyle = patter;
+    context.fillRect(0, 0, 500, 500);
+};
+
+function loadImage(src) {
+    return new Promise((resolve) => {
+        const image = new Image();
+        image.src = src;
+        image.onload = () => resolve(image);
+    });
+}
+
+// const gradient = context.createLinearGradient(0, 0, 500, 500);
+
+// gradient.addColorStop(0, 'red');
+// gradient.addColorStop(0.25, 'black');
+// gradient.addColorStop(0.75, 'white');
+// gradient.addColorStop(1, 'green');
+
+// context.beginPath();
+// context.fillRect(0, 0, 500, 500);
+
+// context.fillStyle = gradient;
+// context.fillRect(0, 0, 500, 500);
+
+// const gradient = context.createRadialGradient(300, 250, 50, 250, 250, 200);
+
+// gradient.addColorStop(0, "red");
+// gradient.addColorStop(0.5, "white");
+// gradient.addColorStop(1, "green");
+
+// context.fillStyle = gradient;
+// context.fillRect(0, 0, 500, 500);
